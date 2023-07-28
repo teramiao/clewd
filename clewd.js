531a906dfc4d8b2f591b2376e1aa9695f634e701
teramiao <140492425+teramiao@users.noreply.github.com>
2023-07-28 14:21:16 +0800

    Update clewd.js

--- clewd.js
+++ clewd.js
@@ -2,7 +2,7 @@
  * SET YOUR COOKIE HERE
  * @preserve
  */
-const Cookies = '_urTw1qeanv8D-1wwkFBERIGgP70E_DTB_WkofZ324-snbWkkIFyi4HqLGjwPvKRuzYqD6REw-2X-WzAAA; intercom-device-id-lupk8zyo=484dcdbf-2b88-4956-9bcb-565e942b0f2e; intercom-session-lupk8zyo=Qlp2Mlh2anBPeEIyQ0ZLTTJ5b09xb1FNS1VkaVlyeGgrOVFtaFRDNmlaQm9TTlVRMnkrQ0F4QVdhdnpSZ1JRRi0tczdjLzJUZGtPd3FkMktQZlZzYUM3dz09--5de6c68ca1d4f3166ee135959e7a3ad229bec1d7';
+const Cookies = '';
 
 /**
 ## EXPERIMENTAL
